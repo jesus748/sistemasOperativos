bd7e2b9842551659e0f30519a28faebd9424096f
mathias <mathias.castillo@correounivalle.edu.co>
2019-07-12 11:26:58 -0500

    se modificó temporal_var.c

--- 2019_07_12/temporal_var.c
+++ 2019_07_12/temporal_var.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int a;
+
 
 int sum(int *x, int y){
 	int z;
@@ -11,7 +11,6 @@ int sum(int *x, int y){
 
 int main (int argc, char** argv){
 	int x1=4,y1=5;
-	a=4;
 	printf("suma: %d\n", sum(&x1,y1));
 	printf("x = %d\n", x1);
 	return 0;
